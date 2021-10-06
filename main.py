3c32bae04d3e1e43a44d3a872c1bf940b7520c5f
Joel Alfaro <alfarojoela@outlook.com>
2021-10-06 10:38:29 -0700

    solution was so simple.  goofed up though.  need to review and refactor code that could have been used as a solution outside of lecture's solution methodology.  many false starts and suboptimal approaches.  learned some more python strategies.  one solution converted csv file into readcsv object and wrote to dictionary.  then used pop to remove headings from column.  added more print statements to follow different methodologies for iterating and loops.  added a few more print statements.

--- main.py
+++ main.py
@@ -56,13 +56,13 @@ import pandas
 
 ######################
 #LECTURE SOLUTION:
-#data = pandas.read_csv("nato_phonetic_alphabet.csv")
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-#phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-#word = input("Enter a word: ").upper()
-#output_list = [phonetic_dict[letter] for letter in word]
-#print(output_list)
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
 
 ######################
 #nato_dict ={}
